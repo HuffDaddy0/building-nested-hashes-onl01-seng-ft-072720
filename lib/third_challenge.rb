895e59e7b00447776e4431e29ad74be014a9410b
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 02:44:00 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,18 +18,18 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet"
         :age => "50"
-        
+
         #your key/value pairs here
         },
       :matriarch => {
         :name => "Lady Capulet"
         :age => "51"
-        
+
         #your key/value pairs here
         },
       :heroine => {
@@ -42,5 +42,5 @@ def third_challenge
    }
   }
 
-  
+
 end
